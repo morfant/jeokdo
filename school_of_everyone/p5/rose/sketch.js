64bcf31284ad3e9eeb6d3e61ce4c2ea7cc4acb10
giy <giy.hands@gmail.com>
2018-09-10 00:14:41 +0900

    Add reose

---
+++ school_of_everyone/p5/rose/sketch.js
@@ -0,0 +1,69 @@
+// https://en.wikipedia.org/wiki/Rose_(mathematics)
+var k, n, d, c, coff;
+var sliderN, sliderD, sliderC;
+var pn, pd, pc;
+var off, offl; // perlin noise offset
+var frameCountDiv = 10; // The lager the slower fluctuating Speed
+
+function setup() {
+    
+    createCanvas(500, 500);    
+    sliderN = createSlider(1, 10, 4, 1);
+    sliderD = createSlider(1, 10, 3, 1);
+    sliderC = createSlider(-200, 200, 0, 1);
+//    pn = createP('');
+//    pn.id('text_n');
+//    pd = createP('');
+//    pd.id('text_d');
+//    pc = createP('');
+//    pc.id('text_c');
+//    pn.position(0, height + 20);
+    off = 0;
+    offl = 0;
+    coff = 0;
+}
+
+
+function draw() {
+    
+    colorMode(RGB);
+    background(0);
+    n = sliderN.value();
+    d = sliderD.value();
+    coff = sliderC.value();
+    
+//    pn.innerHTML = n;
+    
+    k = n/d;
+    
+    
+    
+    translate(width/2, height/2);
+    
+    // offset parameter
+    // https://en.wikipedia.org/wiki/Rose_(mathematics)#Offset_parameter
+    c = sin(frameCount/frameCountDiv) * 60 * noise(off);
+    off += 0.1;
+    
+    
+    beginShape();
+    colorMode(HSB);
+    stroke(165, 20, 100);
+    strokeWeight(13 * noise(offl));
+    offl += 0.4;
+    
+//    noFill();
+    
+    for (var a = 0; a < TWO_PI * d; a += 0.01) {
+        fill(map(n, 1, 10, 0, 255), 70, 60 + c);
+        stroke(165, map(d, 1, 10, 0, 100), 100);
+        var r = 200 * cos(k*a) + (c + coff);
+        var x = r * cos(a);
+        var y = r * sin(a);
+        vertex(x, y);
+    }
+    endShape(CLOSE);
+    
+    
+    
+}
