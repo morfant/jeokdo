de138ef959a593b407215cb0db1f6d4bfd36baee
giy <giy.hands@gmail.com>
2018-09-21 22:53:19 +0900

    Edit 01_prepare

--- school_of_everyone/p5/01_prepare/sketch.js
+++ school_of_everyone/p5/01_prepare/sketch.js
@@ -1,11 +1,13 @@
 function setup() {
     
+    createCanvas(400, 400);
+    background("DarkKhaki");
     
+    multipleCircle(200, 200);
 }
 
 
 function draw() {
-    
     
     
 }
@@ -13,6 +15,19 @@ function draw() {
 
 function multipleCircle (x, y) {
     
+    fill(190, 100, 200);
+    ellipse(x, y, 100, 100);
     
+    fill(90, 100, 200);
+    ellipse(x+10, y+10, 100, 100);
+    
+    fill(10, 10, 200);
+    ellipse(x+20, y+20, 100, 100);
+    
+    fill(190, 10, 20);
+    ellipse(x+30, y+30, 100, 100);
+    
+    fill(90, 10, 100);
+    ellipse(x+40, y+40, 100, 100);
     
 }
