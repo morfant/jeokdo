1fb5dbf26462fbb58dfbdab198aa4219137c1aa4
giy <giy.hands@gmail.com>
2018-10-12 10:51:50 +0900

    ..

--- school_of_everyone/p5/04/g_fractal_tree_random/sketch.js
+++ school_of_everyone/p5/04/g_fractal_tree_random/sketch.js
@@ -1,55 +1,47 @@
 var angle = 0;
-var slider;
 var mul = 0.8;
 var i = 0;
+var redraw = true;
 
 function setup() {
     
 	createCanvas(500, 500);
 	stroke(255);
 
-	slider = createSlider(0, TWO_PI, PI/4, 0.01);
+	angle = PI/4;
 
-	textSize(10);
 
+	noLoop();
 }
-
 
 function draw() {
 
-	noLoop();
-	angle = slider.value();
-
 	background(50);
 	translate(width/2, height);
-	drawBranch(150);
-
-
+	drawBranch(100);
 
 }
 
 
 function drawBranch(len) {
 
-	// i++;
-	// text(i, 0, 0);
-
+	strokeWeight(len/15);
 	line(0, 0, 0, -len);
 	translate(0, -len);
 
 	if (len > 4) {
+		// right
 		var r = random(1);
 		if (r > 0.2) {
-		// right
 			push(); // save
 				rotate(angle * r);
 				drawBranch(len * mul);
 			pop(); // restore
 		}
 
+		// left
 		var r2 = random(1);
 		if (r2 > 0.1) {
-			// left
 			push(); // save
 				rotate(-angle * r2);
 				drawBranch(len * mul * r2);
