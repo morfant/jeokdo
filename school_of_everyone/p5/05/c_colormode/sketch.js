82036b64dcf8c55e128df34e184b6f455665f4cc
giy <giy.hands@gmail.com>
2018-10-13 23:32:51 +0900

    Add 05

---
+++ school_of_everyone/p5/05/c_colormode/sketch.js
@@ -0,0 +1,45 @@
+var n = 0;
+var c = 4;
+var doDraw = true;
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    angleMode(DEGREES);
+    colorMode(HSB);
+    background(0);
+    frameRate(30);
+}
+
+function draw() {
+
+    if (doDraw) {
+        
+//    frameRate(2);
+
+    //    var a = n * 137.3;
+        // var a = n * 137.5;
+       var a = n * 137.6;
+        var r = c * sqrt(n);
+
+        var x = 2 * r * cos(a) + width/2;
+        var y = 2 * r * sin(a) + height/2;
+
+        fill(155, 100 * cos(a/3) * 255, 200);
+        noStroke();
+            
+
+    //    if (n % 3 == 0) {
+           ellipse(x, y, random(4, 8), random(4, 10));    
+    //    }
+
+        n++;
+    }
+}
+
+
+function keyTyped() {
+    if (key === 'p'){
+        doDraw = !doDraw;
+    }
+}
+
