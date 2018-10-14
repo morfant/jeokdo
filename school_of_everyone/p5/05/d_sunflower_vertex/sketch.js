b1033f9af67524fdbf5fc15b27b975594520d036
morfant <giy.hands@gmail.com>
2018-10-14 22:37:54 +0900

    edit 05..

--- school_of_everyone/p5/05/d_sunflower_vertex/sketch.js
+++ school_of_everyone/p5/05/d_sunflower_vertex/sketch.js
@@ -1,51 +1,43 @@
-var n = 0;
 var c = 4;
-var doDraw = true;
+var iterNum = 5000;
+var stepMul = 4;
 
 function setup() {
-    createCanvas(innerWidth, innerHeight);
+    createCanvas(600, 600);
     angleMode(DEGREES);
     colorMode(HSB);
-    background(0);
+    rectMode(CENTER);
 }
 
 function draw() {
-    frameRate(8);    
 
-    if (doDraw) {
+    // frameRate(random(1, 20));    
+    frameRate();    
+    background(0);
 
-        background(0);
-        beginShape();
+    push();
+    translate(width/2, height/2);
+    beginShape();
 
-        var step = noise(frameCount/10);
+    // stroke(25 + 100 * noise(frameCount/20), 70, 100);
+    // strokeWeight(0.1);
+    // noFill();
+    noStroke();
 
-        // stroke(25 + 100 * (sin(i) + 1), 70, 100);
-        stroke(25 + 100 * noise(frameCount/20), 70, 100);
-        var ss = sin(frameCount*10) + 1;
-        // console.log(ss);
-        strokeWeight(0.1 + 1 * (ss)/10);
-        // strokeWeight(0.2 + 1 * (ss));
-        // strokeWeight(0.2 * (sin(frameCount/40) + 1));
-        noFill();
+    var step = noise(frameCount/10);
+    for (var n = 0; n < iterNum; n+=(step*stepMul)) {
 
-        for (var i = 0; i < 5000; i+=(step*3)) {
+        var a = n * 137.5; // theta
+        var r = c * sqrt(n); // 반지름 radius
 
-            var a = i * 127.5;
-            var r = c * sqrt(i);
+        var x = r * cos(a);
+        var y = r * sin(a);
+        // vertex(x, y);
+        // fill(160, 100, 100);
+        fill(160, 100 * sin(n/10), cos(n/10)*100); // S, B를 조절하여 입체감을 줄 수 있다.
+        rect(x, y, 10 + random(50), random(29));
 
-            var x = 1 * r * cos(a) + width/2;
-            var y = 1 * r * sin(a) + height/2;
-            vertex(x, y);
-    
-        }
-        endShape(CLOSE);
     }
+    endShape();
+    pop();
 }
-
-
-function keyTyped() {
-    if (key === 'p'){
-        doDraw = !doDraw;
-    }
-}
-
