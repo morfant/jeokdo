27d6e21bd5be52366f46c0490f04abb818b56d90
morfant <giy.hands@gmail.com>
2018-11-12 16:39:17 +0900

    del files

--- 2018_11_haja/p5/01_prepare/b_function/sketch.js
+++ 2018_11_haja/p5/01_prepare/b_function/sketch.js
@@ -1,12 +1,8 @@
 function setup() {
-    
     createCanvas(400, 400);
     background("DarkKhaki"); // color를 이름으로 정할 수 있다.
     
-    multipleCircle(200, 200);
-
-    multipleCircle(20, 20);
-
+    
 }
 
 
@@ -16,13 +12,9 @@ function draw() {
 }
 
 
+// 함수만들기
 // 크기를 결정하는 아규먼트를 추가해 봅시다.
 function multipleCircle (x, y) {
 
-    // noStroke(); // 이후 외곽선을 그리지 말라는 명령.
-    for (let i = 0; i < 50; i++) {
-        fill(255 - i, 16);
-        ellipse(x, y, 10 + i, 10 * i)
-    }
 
 }
