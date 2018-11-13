c14425d3b5be58e5a71334d55b3361c697ff3a14
morfant <giy.hands@gmail.com>
2018-11-13 19:28:06 +0900

    prepare 03

--- 2018_11_HAJA/p5/03_data/b_weather_walker/sketch.js
+++ 2018_11_HAJA/p5/03_data/b_weather_walker/sketch.js
@@ -1,4 +1,5 @@
-var walkers = [];
+let weather;
+let walkers = [];
 
 class Walker{
     constructor(_size, _rand_x, _rand_y, _col) {
@@ -10,18 +11,29 @@ class Walker{
         this.rand_y = _rand_y;
     }
 
-    move() {
-        this.x = this.x + random(-this.rand_x, this.rand_x);
-        this.y = this.y + random(-this.rand_y, this.rand_y);
+    move(wind) {
+        this.x = this.x + random(-this.rand_x, this.rand_x) + wind.x;
+        this.y = this.y + random(-this.rand_y, this.rand_y) + wind.y;
+
+        if (this.x < 0) this.x = width;
+        if (this.x > width) this.x = 0;
+        if (this.y < 0) this.y = height;
+        if (this.y > height) this.y = 0;
     }
 
-    show() {
-        stroke(255);
-        strokeWeight(3);
-        noStroke();
-        fill(155, 100, this.color);
+    show(temp) {
+        colorMode(HSB);
+        let hue = map(temp, -10, 40, 240, 0);
+        fill(hue, 100, 100, 0.5);
         ellipse(this.x, this.y, this.size, this.size);
     }
+}
+
+function preload() {
+
+    let url = 'https://api.apixu.com/v1/current.json?key=96a9971dd31a456e9ce103938180411&q=NYC';
+    weather = loadJSON(url);
+
 }
 
 function setup() {
@@ -35,11 +47,30 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+
+    let place = weather.location.name;
+    let temp = weather.current.temp_c;
+    let isDay = weather.current.is_day;
+    let windDegree = weather.current.wind_degree;
+    let windKPH = weather.current.wind_kph;
+    let rain = weather.current.precip_mm;
     
-    for (let i = 0; i < 200; i++) {
-        walkers[i].move();
-        walkers[i].show();
+    let windVector = p5.Vector.fromAngle(radians(windDegree), windKPH/2);
+
+    colorMode(RGB);
+    textSize(40);
+    if (isDay == 0) {
+        background(0);
+        fill(255);
+    } else {
+        background(255);
+        fill(0);
+    }
+    text(place, width/2, 100);
+
+   for (let i = 0; i < 200; i++) {
+        walkers[i].move(windVector);
+        walkers[i].show(temp);
     }
     
 }
